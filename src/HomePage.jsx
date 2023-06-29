6a51c997b69fbf092554e2c42a5fddc6cfac8dd7
minaskoric <mina.skoric99@gmail.com>
2023-06-30 01:07:09 +0200

    dodat image carousel na pocetnu stranicu

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './HomePage.css';
  
-
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 const HomePage = () => {
   return (
     <div className="home-page">
       <h1>Dobrodošli na Kanban Tablu</h1>
+      <div className="carousel-container">
+        <Carousel showArrows={true} infiniteLoop={true}>
+          <div>
+            <img src="https://www.shutterstock.com/shutterstock/photos/1337303492/display_1500/stock-photo-scrum-task-board-on-dark-wall-in-office-1337303492.jpg" alt="Kanban slika 1" />
+            <p className="legend">Slika 1</p>
+          </div>
+          <div>
+            <img src="https://www.shutterstock.com/image-vector/kanban-project-management-system-flat-600w-1255748305.jpg" alt="Kanban slika 2" />
+            <p className="legend">Slika 2</p>
+          </div>
+          <div>
+            <img src="https://www.shutterstock.com/image-vector/online-kanban-technology-laptop-paper-600w-1143831317.jpg" alt="Kanban slika 3" />
+            <p className="legend">Slika 3</p>
+          </div>
+        </Carousel>
+      </div>
       <div className="kanban-info">
         <h2>Šta je Kanban tabla?</h2>
         <p>
